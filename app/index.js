b3546b126adf4155414d33cc2960c3b422bd4688
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-02-11 17:18:31 -0300

    Terminando o carrossel

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
-var email = window.document.getElementById('email')
-var senha = window.document.getElementById('senha')
+function verificacao() {
+    const email_usuario = window.document.getElementById("email_usuario")
+    const senha_usuario = window.document.getElementById("senha_usuario")
 
+    const dados = localStorage.getItem("contas")
+    const result = JSON.parse(dados)[0].email
+
+    if(email_usuario == dados.find(element => element = email_usuario))
+        alert("sucesso")
+    else
+        alert("tente de novo")
+
+}
